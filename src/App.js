5bcf4da4b2aada5ae9f6ebd2cc531e864b10db44
PouriaDamavandi <damavandipouria@gmail.com>
2022-03-19 23:42:20 +0330

    finished Banner and Cards with their styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Cards from './components/Cards'
+
 function App() {
   return (
     <div >
       <header>
      <Navbar/>
      <Banner/>
+     <Cards />
       </header>
     </div>
   );
