cb4418e0b3913587ea49aeda5f974d5529cbbd2b
PouriaDamavandi <damavandipouria@gmail.com>
2022-03-30 03:40:19 +0430

    Added counter calculation to the cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./Card.module.css";
-import down from "../images/down.svg"
-import up from "../images/up.svg"
+import down from "../images/down.svg";
+import up from "../images/up.svg";
 
 class Card extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            counter: 0,
-        }
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  downHandler = () => {
+    if (this.state.counter >= 1) {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
     }
-    downHandler = () =>{
-      if( this.state.counter >= 1){
-      this.setState(prevState =>({
-        counter : prevState.counter - 1,
-      }) ) 
-    }}
-    upHandler = () => {
-      this.setState(prevState =>({
-        counter : prevState.counter + 1,
-      }))
-    }
-    render() {
-        const {image, name, price} = this.props;
-        const {counter} = this.state;
-        return (
-            <div className={styles.container}>
-                <img src={image} alt="Laptop" />
-                <h3>{name}</h3>
-                <p>{price}</p>
-                       <div className={styles.counter}>
-       <img className={this.state.counter ? "" : styles.deactive }src={down} alt="Arrow Down" onClick={this.downHandler}/>
-       <span>{counter}</span>
-       <img src={up} alt="Arrow Up" onClick={this.upHandler}/>
-            </div>
-            </div>
-        );
-    }
+  };
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+  render() {
+    const { image, name, price } = this.props;
+    const { counter } = this.state;
+    return (
+      <div className={styles.container}>
+        <img src={image} alt="Laptop" />
+        <h3>{name}</h3>
+        <p>
+          {price}
+          {counter
+            ? `* ${counter}= ${counter * Number(price.split(" ")[0])} $`
+            : ""}{" "}
+        </p>
+        <div className={styles.counter}>
+          <img
+            className={!counter && styles.deactive}
+            src={down}
+            alt="Arrow Down"
+            onClick={this.downHandler}
+          />
+          <span>{counter}</span>
+          <img src={up} alt="Arrow Up" onClick={this.upHandler} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
