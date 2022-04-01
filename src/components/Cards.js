d7da0d718ae7466147d51b04e5790d1542c500c3
PouriaDamavandi <damavandipouria@gmail.com>
2022-04-01 08:14:09 +0430

    Added List Rendering by using Map

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Cards.module.css";
 import Card from "./Card";
 import laptop1 from "../images/laptop1.jpg";
 import laptop2 from "../images/laptop2.jpg";
 import laptop3 from "../images/laptop3.jpg";
 import laptop4 from "../images/laptop4.jpg";
+import { v4 } from "uuid";
 
-const Cards = () => {
-  return (
-    <div className={styles.container}>
-      <Card image={laptop1} name={"Sony"} price={"1150 $"} />
-      <Card image={laptop2} name={"HP"} price={"1050 $"} />
-      <Card image={laptop3} name={"Macbook"} price={"1200 $"} />
-      <Card image={laptop4} name={"ASUS"} price={"1100 $"} />
-    </div>
-  );
-};
+class Cards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      phoneData: [
+        { image: laptop1, name: "Sony", price: "1150 $" },
+        { image: laptop2, name: "HP", price: "1050 $" },
+        { image: laptop3, name: "Macbook", price: "1200 $" },
+        { image: laptop4, name: "ASUS", price: "1100 $" },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        {/* <Card image={laptop1} name={"Sony"} price={"1150 $"} />
+        <Card image={laptop2} name={"HP"} price={"1050 $"} />
+        <Card image={laptop3} name={"Macbook"} price={"1200 $"} />
+        <Card image={laptop4} name={"ASUS"} price={"1100 $"} /> */}
+        {this.state.phoneData.map(phone => <Card key={v4()} image={phone.image} name={phone.name} price={phone.price} />)}
+      </div>
+    );
+  }
+}
 
 export default Cards;
